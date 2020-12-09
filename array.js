555dc6581320878e01e8f4474e7825cbdbda9d45
preethi <fabreztina@gmail.com>
2020-12-09 22:24:50 +0530

    adding array.js file

---
+++ array.js
@@ -0,0 +1,13 @@
+var array=[],len=4,i;
+for(i=0;i<=len;i++){
+    array[i]=prompt("enter array elements:");
+}
+console.log(array);
+
+
+//array square
+
+for(i=0;i<=len;i++){
+    array[i]=array[i]*array[i];
+}
+console.log(array);
