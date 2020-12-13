688652b6789c5019fd5e3c48faec729c75198b38
preethi <fabreztina@gmail.com>
2020-12-13 11:39:12 +0530

    adding sidebysidemultab.js

--- sidebysidemultab.js
+++ sidebysidemultab.js
@@ -2,7 +2,7 @@ var i,j,k,n=5;
 var array=new Array(5);
 for(i=1;i<=10;i++){
     for(j=1;j<=n;j++){
-        array[j]=j*i;
+        console.log(array[j]=`${j}*${i}=${j*i}`);
     }
     
     console.log(array.join(" "+" "+" "));
