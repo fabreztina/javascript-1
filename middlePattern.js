6ffb49859eb7348c2db49d7cbb715f9766ff9397
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-12-13 13:07:19 +0530

    middle pattern - method 2

--- middlePattern.js
+++ middlePattern.js
@@ -1,15 +1,64 @@
 function middlePattern(n){
-    let str=""
-    for(let i=1;i<=n;i++){
-        for(let k=1;k<=(n-i);k++){
-            str+=" ";
+    let str = ''
+    const length = n+(n-1)
+    let startValue = Math.floor(length/2)
+    
+    for(let i = 1; i <= n; i++){
+        let count = i
+        let nextValue = startValue + 2
+        for(let j = 0; j < length; j++){
+            if(count > 0 ){
+                if(j === startValue){
+                    str += '*'
+                    count --
+                }
+                else if(j === nextValue){
+                    str += '*'
+                    count --
+                    nextValue += 2
+                }
+                else{
+                    str += ' '
+                }
+            }
+            else{
+                str += ' '
+            }
         }
-        for(let j=1;j<=i;j++){
-            str+="* ";
-        }
-        str+="\n";
+        startValue--
+        str += '\n'
     }
     console.log(str)
 }
-
 middlePattern(7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function middlePattern(n){
+//     for(let i = 1; i <= n; i++){
+//         let str = ''
+//         for(let j = n; j >= 1; j--){
+//             let spaceCount = j - i
+//             if(spaceCount > 0){
+//                 str += ' '
+//             }
+//             else{
+//                 str += '* '
+//             }
+//         }
+//         console.log(str.slice(0, str.length-1))
+//     }
+// }
+
+// middlePattern(10)
