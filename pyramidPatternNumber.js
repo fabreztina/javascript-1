d71f7bfc14907f5ff13d9214c2754b59b2e3ffad
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-12-13 15:45:04 +0530

    Pyramid Number Pattern

---
+++ pyramidPatternNumber.js
@@ -0,0 +1,32 @@
+function pyramidPatternNumber(n){
+    let str = ''
+    const length = n+(n-1)
+
+    for(let i = n; i > 0; i--){
+        let count = n - i + 1 
+        let temp = i + 2
+        for(let j = 1; j <= length; j++){
+            if(count > 0){
+                if(j === i){
+                    str += `${n - i + 1}`
+                    count--
+                }
+                else if(j === temp){
+                    str += `${n - i + 1}`
+                    count--
+                    temp += 2
+                }
+                else{
+                    str += ' '
+                }
+            }
+            else{
+                str += ' '
+            }
+        }
+        str += '\n'
+    }
+    console.log(str)
+}
+
+pyramidPatternNumber(6)
