474ec285a6642c34bd219931941db7f4c836fb10
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-12-21 19:27:51 +0530

    added condition

--- factorialWithMapAndReduce.js
+++ factorialWithMapAndReduce.js
@@ -1,6 +1,8 @@
 function factorialWithMapAndReduce(n){
-    return Array.from({length: n})
-    .map((v,i) => i + 1)
-    .reduce((acc,cur) => acc * cur, 1)
+    if(typeof n === 'number'){
+        return Array.from({length: n})
+        .map((v,i) => i + 1)
+        .reduce((acc,cur) => acc * cur, 1)
+    }
 }
 console.log(factorialWithMapAndReduce(6))
