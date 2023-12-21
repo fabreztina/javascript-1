d15a9ac1530aa1356da764e56cd6be6bc2b3506f
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-12-21 12:40:49 +0530

    Todo - sum of positive numbers

--- sumOfPositiveNumbers.js
+++ sumOfPositiveNumbers.js
@@ -1,1 +1,10 @@
-function sumOf
+function sumOfPositiveNumbers(arr){
+    return arr.reduce((acc,cur) => {
+        if(cur >= 0){
+            acc += cur
+        }
+        return acc
+    }, 0)
+}
+console.log(sumOfPositiveNumbers([1, -4, 12, 0, -3, 29, -150]))
+console.log(sumOfPositiveNumbers([-1,-55,-9,-3]))
