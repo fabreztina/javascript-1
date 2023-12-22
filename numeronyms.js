a61bf32afeefdd0beb73e9068f2f6ad95511fd0c
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-12-22 10:35:32 +0530

    used map() instead of reduce()

--- numeronyms.js
+++ numeronyms.js
@@ -1,14 +1,5 @@
 function numeronyms(str){
-    return str.split(' ').reduce((acc,cur) => {
-        if(cur.length > 3){
-            acc +=  cur[0] + `${cur.length - 2}` + cur[cur.length - 1]
-            acc += ' '
-        }
-        else{
-            acc += cur
-            acc += ' '
-        }
-        return acc
-    },'')
+    return str.split(' ').map(v => v.length > 3 ? v[0] + `${v.length - 2}` + v[v.length - 1] : v)
+    .join(' ')
 }
 console.log(numeronyms("Every developer likes to mix kubernetes and javascript"))
